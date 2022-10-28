762af571e1ff8b546798c501beee236dedd428fd
pwoosam <pwoosam5@icloud.com>
2022-10-27 19:34:18 -0700

    Disable staking rewards button until mirror node issue on github is fixed

--- frontend/src/components/accounts/account-balance.tsx
+++ frontend/src/components/accounts/account-balance.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import { TokenInfo, TokenInfoType } from "../../api-clients/hedera-mirror-node-api-client";
 import { getTokenInfo } from "../../api-clients/hedera-mirror-node-api-helper";
 import { getAccountBalances } from "../../services/hedera-client";
-import { StakingRewardsButton } from "./account-staking-rewards";
+// import { StakingRewardsButton } from "./account-staking-rewards";
 
 interface TokenBalance {
   tokenInfo: TokenInfo | "HBAR",
@@ -70,12 +70,12 @@ export const AccountBalance = (props: {
         >
           Token Balances
         </Typography>
-        <StakingRewardsButton
+        {/* <StakingRewardsButton
           accountId={accountId}
           sx={{
             marginLeft: "auto"
           }}
-        />
+        /> */}
       </Box>
       {isLoading ? (
         <CircularProgress />
