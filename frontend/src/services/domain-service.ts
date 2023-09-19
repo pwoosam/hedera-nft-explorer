6053f9e90c75c1d39e1829a6f93382985ea020a4
aconstas <82304192+aconstas@users.noreply.github.com>
2023-09-19 13:10:50 -0700

    comment out getAccountIdFromDomain export
    Retained getAccountIdFromDomain() for potential future use if hbar domains return to a usable state.

--- frontend/src/services/domain-service.ts
+++ frontend/src/services/domain-service.ts
@@ -3,9 +3,10 @@ import { Resolver, ICache } from '@pwoosam/hashgraph-name-resolution-sdk';
 const domainResolver = new Resolver('hedera_main');
 domainResolver.init();
 
-export const getAccountIdFromDomain = async (domain: string): Promise<string | undefined> => {
-  return await domainResolver.resolveSLD(domain);
-}
+// Unused for now due to the state of .hbar domains
+// export const getAccountIdFromDomain = async (domain: string): Promise<string | undefined> => {
+//   return await domainResolver.resolveSLD(domain);
+// }
 
 export const getDomainsForAccount = async (accountIdOrDomain: string): Promise<string[]> => {
   await domainResolver.isCaughtUpPromise;
